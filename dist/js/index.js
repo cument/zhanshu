2e51ab51f4836438356e9a9eab6c8b156cf33eff
likx <704989929@qq.com>
2017-02-22 16:51:30 +0800

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,19 @@
+webpackJsonp([0,1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	/**
+	 * Created by Administrator on 2017/2/22.
+	 */
+	"use strict";
+
+/***/ }
+]);
+//# sourceMappingURL=index.js.map
